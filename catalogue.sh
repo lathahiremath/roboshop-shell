6fab7bb85f3b05264edffe532c3d74041b84469d
Latha <lathahiremath03@gmail.com>
2023-04-19 17:15:38 +0530

    corrected mongo to mongodb

--- catalogue.sh
+++ catalogue.sh
@@ -38,4 +38,4 @@ echo -e "\e[36m>>>>>install mongodb client<<<<<\e[0m"
 yum install mongodb-org-shell -y
 
 echo -e "\e[36m>>>>>Load schema<<<<<\e[0m"
-mongo --host mongo-dev.latha.fun </app/schema/catalogue.js
+mongo --host mongodb-dev.latha.fun </app/schema/catalogue.js
