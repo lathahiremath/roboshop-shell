aa5e9bcf6c6cb7bec88beec0f15d025781bfff08
Latha <lathahiremath03@gmail.com>
2023-04-19 20:15:53 +0530

    there are two conf file so add one more

--- redis.sh
+++ redis.sh
@@ -5,7 +5,7 @@ echo -e "\e[36m>>>>>install redis<<<<<\e[0m"
 dnf module enable redis:remi-6.2 -y
 yum install redis -y
 
-echo -e "\e[36m>>>>>update redis listen adress<<<<<\e[0m"
+echo -e "\e[36m>>>>>update Redis listen adress<<<<<\e[0m"
 sed -i -e 's|127.0.0.0|0.0.0.0|' /etc/redis.conf /etc/redis/redis.conf
 
 echo -e "\e[36m>>>>>start redis service<<<<<<\e[0m"
