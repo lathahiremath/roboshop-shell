f2972c41379b9233a86eb6fd859d006b204eb50d
Latha <lathahiremath03@gmail.com>
2023-04-20 17:19:13 +0530

    cart service

--- cart.sh
+++ cart.sh
@@ -29,4 +29,4 @@ cp /home/centos/roboshop-shell/cart.service /etc/systemd/system/cart.service
 echo -e "\e[36m>>>>>start cart service<<<<<\e[0m"
 systemctl daemon-reload
 systemctl enable cart
-systemctl start cart
+systemctl restart cart
