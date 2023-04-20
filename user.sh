45ad2da72e0156c6eedf5b8787e37f0d03e99d18
Latha <lathahiremath03@gmail.com>
2023-04-20 15:32:03 +0530

    creating user script

--- user.sh
+++ user.sh
@@ -1,16 +1,41 @@
+echo -e "\e[36m>>>>>configuring nodejs repos<<<<<\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+
+echo -e "\e[36m>>>>>install Nodejs <<<<<\e[0m"
 yum install nodejs -y
+
+echo -e "\e[36m>>>>>Add application user<<<<<\e[0m"
 useradd roboshop
+
+echo -e "\e[36m>>>>>create application directory<<<<<\e[0m"
+rm -rf /app
 mkdir /app
-curl -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
+
+echo -e "\e[36m>>>>>Download app content<<<<<\e[0m"
+curl -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+
 cd /app
+
+echo -e "\e[36m>>>>>Unzip app content<<<<<\e[0m"
 unzip /tmp/user.zip
 cd /app
+
+echo -e "\e[36m>>>>>install Nodejs dependencies<<<<<\e[0m"
 npm install
-cp user.service /etc/systemd/system/user.service
+
+echo -e "\e[36m>>>>>create application directory<<<<<\e[0m"
+cp /home/centos/roboshop-shell/user.service /etc/systemd/system/user.service
+
+echo -e "\e[36m>>>>>start user service<<<<<\e[0m"
 systemctl daemon-reload
 systemctl enable user
 systemctl start user
+
+echo -e "\e[36m>>>>>copy mongodb repo<<<<<\e[0m"
 cp mongo.repo /etc/yum.repos.d/mongo.repo
+
+echo -e "\e[36m>>>>>install mongodb client<<<<<\e[0m"
 yum install mongodb-org-shell -y
+
+echo -e "\e[36m>>>>>load schema<<<<<\e[0m"
 mongo --host mongo-dev.latha.fun </app/schema/user.js
