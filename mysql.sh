96b4e08e51baf6d18dddcfab3b5e99f4cb41d87e
Latha <lathahiremath03@gmail.com>
2023-04-20 17:30:19 +0530

    mysql  service

--- mysql.sh
+++ mysql.sh
@@ -1,6 +1,15 @@
+echo -e "\e[36m>>>>>disable mysql 8 version<<<<<\e[0m"
 dnf module disable mysql -y
-cp mysql.repo /etc/mysql.repos.d/mysql.repo
+
+echo -e "\e[36m>>>>copy mysql repo file<<<<<\e[0m"
+cp /home/centos/roboshop-shell/mysql.repo /etc/mysql.repos.d/mysql.repo
+
+echo -e "\e[36m>>>>>Install Mysql<<<<<\e[0m"
 yum install mysql-community-server -y
+
+echo -e "\e[36m>>>>>Start mysql<<<<<\e[0m"
 systemctl enable mysqld
-systemctl start mysqld
+systemctl restart mysqld
+
+echo -e "\e[36m>>>>>Reset mysql password<<<<<\e[0m"
 mysql_secure_installation --set-root-pass RoboShop@1
