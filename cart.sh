9c6d22cc39e4de83677c7459260cf19b3aa60cfe
Latha <lathahiremath03@gmail.com>
2023-04-22 20:49:27 +0530

    cart  service

--- cart.sh
+++ cart.sh
@@ -12,7 +12,7 @@ rm -rf /app
 mkdir /app
 
 echo -e "\e[36m>>>>>Download app content<<<<<<\e[0m"
-curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip
+curl -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip
 
 cd /app
 
