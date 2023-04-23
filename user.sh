ae2a345c4d1bad8e4b16887d8f35e46054b375ab
Latha <lathahiremath03@gmail.com>
2023-04-23 18:22:37 +0530

    create script_path and source file common.sh

--- user.sh
+++ user.sh
@@ -1,7 +1,6 @@
 
 script_path=$(dirname $0)
 source ${script_path}/common.sh
-echo $app_user
 exit
 
 echo -e "\e[36m>>>>>configuring nodejs repos<<<<<\e[0m"
@@ -30,7 +29,7 @@ echo -e "\e[36m>>>>>install Nodejs dependencies<<<<<\e[0m"
 npm install
 
 echo -e "\e[36m>>>>>create application directory<<<<<\e[0m"
-cp /home/centos/roboshop-shell/user.service /etc/systemd/system/user.service
+cp $script_path/user.service /etc/systemd/system/user.service
 
 echo -e "\e[36m>>>>>start user service<<<<<\e[0m"
 systemctl daemon-reload
