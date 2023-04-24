759fb872cd23254044f0f2a736a710ff19d852f1
Latha <lathahiremath03@gmail.com>
2023-04-24 12:22:18 +0530

    component=user func_nodejs

--- common.sh
+++ common.sh
@@ -2,3 +2,38 @@ app_user=roboshop
 
 script=$(realpath "$0")
 script_path=$(dirname $"script")
+
+func_nodejs() {
+echo -e "\e[36m>>>>>configuring nodejs repos<<<<<\e[0m"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+
+echo -e "\e[36m>>>>>install Nodejs <<<<<\e[0m"
+yum install nodejs -y
+
+echo -e "\e[36m>>>>>Add application user<<<<<\e[0m"
+useradd ${app_user}
+
+echo -e "\e[36m>>>>>create application directory<<<<<\e[0m"
+rm -rf /app
+mkdir /app
+
+echo -e "\e[36m>>>>>Download app content<<<<<<\e[0m"
+curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
+
+cd /app
+
+echo -e "\e[36m>>>>>Unzip app content<<<<<\e[0m"
+unzip /tmp/${component}.zip
+cd /app
+
+echo -e "\e[36m>>>>>install Nodejs dependencies<<<<<\e[0m"
+npm install
+
+echo -e "\e[36m>>>>>create application directory<<<<<\e[0m"
+cp $(script_path)/${component}.service /etc/systemd/system/${component}.service
+
+echo -e "\e[36m>>>>>start cart service<<<<<\e[0m"
+systemctl daemon-reload
+systemctl enable ${component}
+systemctl restart ${component}
+}
--- user.sh
+++ user.sh
@@ -3,39 +3,8 @@ script_path=$(dirname $"script")
 
 source ${script_path}/common.sh
 
-
-echo -e "\e[36m>>>>>configuring nodejs repos<<<<<\e[0m"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-
-echo -e "\e[36m>>>>>install Nodejs <<<<<\e[0m"
-yum install nodejs -y
-
-echo -e "\e[36m>>>>>Add application user<<<<<\e[0m"
-useradd ${app_user}
-
-echo -e "\e[36m>>>>>create application directory<<<<<\e[0m"
-rm -rf /app
-mkdir /app
-
-echo -e "\e[36m>>>>>Download app content<<<<<\e[0m"
-curl -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
-
-cd /app
-
-echo -e "\e[36m>>>>>Unzip app content<<<<<\e[0m"
-unzip /tmp/user.zip
-cd /app
-
-echo -e "\e[36m>>>>>install Nodejs dependencies<<<<<\e[0m"
-npm install
-
-echo -e "\e[36m>>>>>create application directory<<<<<\e[0m"
-cp $script_path/user.service /etc/systemd/system/user.service
-
-echo -e "\e[36m>>>>>start user service<<<<<\e[0m"
-systemctl daemon-reload
-systemctl enable user
-systemctl restart user
+component=user
+func_nodejs
 
 echo -e "\e[36m>>>>>copy mongodb repo<<<<<\e[0m"
 cp $(script_path)/mongo.repo /etc/yum.repos.d/mongo.repo
