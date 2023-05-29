9758b38e8c965c7001b4ad05fb9efe1fc572c7a2
Latha <lathahiremath03@gmail.com>
2023-05-29 15:40:30 +0530

    functions

--- common.sh
+++ common.sh
@@ -10,13 +10,13 @@ func_print_head() {
 }
 
 func_stat_check() {
-if [ $1 -eq 0 ]; then
-      echo -e "\e[32mSUCCESS\e[0m"
-    else
-      echo -e "\e[31mFAILURE\e[0m"
-          echo "Refer the log file /tmp/roboshop.log for more information"
-          exit 1
-fi
+  if [ $1 -eq 0 ]; then
+    echo -e "\e[32mSUCCESS\e[0m"
+  else
+    echo -e "\e[31mFAILURE\e[0m"
+    echo "Refer the log file /tmp/roboshop.log for more information"
+    exit 1
+  fi
 }
 
 func_schema_setup()
